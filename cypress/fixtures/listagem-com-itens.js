c24dabcbf3e54925e21b80e29f8e35b8fa15b910
Julliana do Couto Almeida <f666546@corp.caixa.gov.br>
2022-03-26 08:57:41 -0300

    adicionando cadastro e listagem

---
+++ cypress/fixtures/listagem-com-itens.js
@@ -0,0 +1,32 @@
+[
+    {
+        "firstName": "Julliana",
+        "lastName": "Almeida",
+        "adress": "sem endereço",
+        "emailAdress": "test@mail.com",
+        "radioGender": "n",
+        "checks": [
+            "Netflix",
+            "Dormir"
+        ],
+        "selectCountries": "Dinamarca",
+        "selectYears": "2006",
+        "selectMonths": "Janeiro",
+        "pwd": "Alunos@21"
+    },
+    {
+        "firstName": "Agilizei",
+        "lastName": "bootcamp",
+        "adress": "sem endereço",
+        "emailAdress": "test@mail.com",
+        "radioGender": "p",
+        "checks": [
+            "Netflix",
+            "Dormir"
+        ],
+        "selectCountries": "Dinamarca",
+        "selectYears": "2006",
+        "selectMonths": "Janeiro",
+        "pwd": "Alunos@21"
+    }
+]
---
+++ cypress/integration/1-getting-started/cadastro.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+var Chance = require('chance')
+var chance = new Chance()
+
+describe('Cadastro', () => {
+    it('Quando eu informar os dados e finalizar, então o cadastro deve ser efetuado', ()=>{
+        cy.visit('https://form-agilizei.netlify.app/');
+      
+        cy.get('input[name=firstName]').type(chance.first())
+        cy.get('input[name=lastName]').type(chance.last())
+        cy.get('textarea[name=adress]').type(chance.address())
+        cy.get('input[name=emailAdress]').type(chance.email())
+        
+        cy.get('input[value=n]').check();
+        cy.get('input[type=checkbox]').check( 'Netflix');
+        cy.get('input[type=checkbox]').check( 'Dormir');
+      
+        cy.get('select#countries').select('Dinamarca', {force: true})
+        cy.get('select#years').select('2006', {force: true})
+        cy.get('select#months').select('Janeiro', {force: true})
+        cy.get('select#days').select('8', {force: true})
+
+        cy.get('input#firstpassword').type('Alunos@21')
+        cy.get('input#secondpassword').type('Alunos@21')
+
+        cy.contains('Finalizar cadastro').click()
+   
+        //deve conter listagem
+        // should contains listagem
+        cy.url().should('contain', 'listagem')
+   
+    });
+});
